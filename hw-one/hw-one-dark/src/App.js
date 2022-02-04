3c26f03dcf992ab792453b663b02f19c5bf13c02
ClareSegrue <cesegrue@gmail.com>
2022-02-03 22:27:50 -0500

    header edits

--- hw-one/hw-one-dark/src/App.js
+++ hw-one/hw-one-dark/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
-import "./shoe.png";
+import logo from "./shoe.png";
+
 
 
 function App() {
@@ -10,11 +11,11 @@ function App() {
       <header className="App-header">
         
         <div className="button-container">
-          <button className="dark-button"><b>Dark</b></button>
-          <button className="light-button"><b>Light</b></button>
+          <button className="dark-button"><b>DARK</b></button>
+          <button className="light-button"><b>LIGHT</b></button>
         </div>
-        <div className="header-title">COB'S COBBLERS
-        <img src="shoe.png" width="32" height="32" alt="x"></img>
+        <div className="header-title">cob's cobblers
+        <img src={logo} width="32" height="32" alt="x"></img>
         </div>
         
         
@@ -24,7 +25,9 @@ function App() {
           left panel
         </div>
         <div className="main-panel">
+          <darkTC></darkTC>
           main panel
+          <darkTC></darkTC>
         </div>
         <div className="right-panel">
           right panel
---
+++ hw-one/hw-one-dark/src/darkTC.js
@@ -0,0 +1,13 @@
+
+import './dark.css';
+import React from "react";
+import logo from "./shoe.png";
+
+
+function darkTC() {
+  return (
+   <div>blah</div>
+  );
+}
+
+export default darkTC;
