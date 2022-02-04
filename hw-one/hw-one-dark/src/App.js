0060a3f8d58e522f9de05dc501ee284d75e7abfd
ClareSegrue <cesegrue@gmail.com>
2022-02-03 21:42:23 -0500

    Adding Header, Panels

--- hw-one/hw-one-dark/src/App.js
+++ hw-one/hw-one-dark/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "./shoe.png";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          BEEEEEP
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Rekt
-        </a>
+        
+        <div className="button-container">
+          <button className="dark-button"><b>Dark</b></button>
+          <button className="light-button"><b>Light</b></button>
+        </div>
+        <div className="header-title">COB'S COBBLERS
+        <img src="shoe.png" width="32" height="32" alt="x"></img>
+        </div>
+        
+        
       </header>
+      <div className="flex-table">
+        <div className="left-panel">
+          left panel
+        </div>
+        <div className="main-panel">
+          main panel
+        </div>
+        <div className="right-panel">
+          right panel
+        </div>
+      </div>
     </div>
   );
 }
