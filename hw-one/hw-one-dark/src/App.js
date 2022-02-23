a17eb2c673ec376f339fcfcbc3a6b176bbd52659
ClareSegrue <cesegrue@gmail.com>
2022-02-23 00:07:34 -0500

    Making it pretttty

--- hw-one/hw-one-dark/src/App.js
+++ hw-one/hw-one-dark/src/App.js
@@ -566,7 +566,7 @@ class App extends Component {
             <Shipping></Shipping>
           </div>
         </Accordion>
-        
+
 
         <form id="user-accept-form" className="temp">
           <div className='user-accept-form'>
@@ -578,7 +578,7 @@ class App extends Component {
             <label for="terms-accept"> I accept the <a href={termsOfPurchase} download>terms and conditions of purchase.</a></label>
             <input type="checkbox" id="terms-accept" name="terms-accept" value="I agree." className='form-flex'></input>
           </div>
-          <input type="submit"></input>
+          <input type="submit" id="submit-button"></input>
         </form>
       </div>
 
@@ -589,7 +589,10 @@ class App extends Component {
   updateLightTwo = () => {
     this.setState(
       <div>
+        <h1>PAYMENT</h1>
         <Payment></Payment>
+        <h1>SHIPPING</h1>
+        <Shipping></Shipping>
 
         <div id="receipt">
           <b>Itemized Total</b>
@@ -599,21 +602,38 @@ class App extends Component {
             <li>$10.00<em> --- Flat Rate Shipping (US)</em></li>
             <li>$10.69<em> --- Taxes</em></li>
           </ul>
-          ----------------------------------------- <br></br>
+          ______________________________ <br></br>
           <b>Final Total: $93.07</b>
         </div>
 
         <form id="user-accept-form" className="temp">
           <div className='user-accept-form'>
-            <label for="payment-accept"> I agree to be billed <b>$93.07</b> and solemnly swear to adhere to law of no backsises!</label>
+            <label for="payment-accept"> I agree to be billed <b>$93.07</b>.</label>
             <input type="checkbox" id="payment-accept" name="payment-accept" value="I agree." className='form-flex'></input>
+          </div>
+          <div id="termsOfPurchase">
+            <b>TERMS AND CONDITIONS OF PURCHASE</b><br></br>
+            <br></br>
+            By accepting this agreement you enter into a binding and eternal contract with the ruinous powers to
+            engage in full and total custodianship of <b id="warning">(1) human soul</b>, granted to you in exchange for the payment
+            described in your purchase summary. You will also be <b>mailed a frozen fish</b> as described to you in the
+            listing. This fish, henceforth called the “fish vessel”, represents the binding terms of your
+            custodianship, and <b id="warning">must be kept frozen at all times</b> to avoid the risk of an all-consuming flame that can
+            never be quenched. <br></br><br></br><b id="warning">You hereby indemnify Cob’s of any harm caused by allowing the fish vessel to
+            thaw, including but not limited to property damage, incineration, and the formation of gaping maws of
+            despair on your property.</b><br></br><br></br>
 
+            By consuming the fish vessel, you <b id="warning">forfeit any rights to the above contracted soul</b>, and agree to render
+            your soul onto Cob’s, or our chosen agent, upon your expiration. To cancel this contract at any time,
+            simply <b id="warning">resurrect the fish vessel using the necromantic arts</b>, then cast the living fish vessel into a suitable
+            void, no less than 24 miles deep. Attach a notarized copy of your name, social security number, order
+            number, and exact second of birth to ensure processing of your cancellation.
           </div>
           <div className='user-accept-form'>
-            <label for="terms-accept"> I accept the <a href={termsOfPurchase} download>terms and conditions of purchase.</a></label>
+            <label for="terms-accept"> I accept the terms and conditions of purchase.</label>
             <input type="checkbox" id="terms-accept" name="terms-accept" value="I agree." className='form-flex'></input>
           </div>
-          <input type="submit"></input>
+          <input type="submit" id="submit-button"></input>
         </form>
 
 
--- hw-one/hw-one-dark/src/components/Shipping.js
+++ hw-one/hw-one-dark/src/components/Shipping.js
@@ -10,19 +10,19 @@ class Shipping extends React.Component {
         <form id="shipping-form">
           <div className='form-flex'>
             <input type="text" id="address-input"></input>
-            <label for="address-input">Address</label>
+            <label for="address-input">Address:</label>
           </div>
           <div className='form-flex'>
             <input type="text" id="city-input" value="Shaker Heights"></input>
-            <label for="city-input">City</label>
+            <label for="city-input">City:</label>
           </div>
           <div className='form-flex'>
             <input type="text" id="country-input" value="2022-06"></input>
-            <label for="country-input">Country</label>
+            <label for="country-input">Country:</label>
           </div>
           <div className='form-flex'>
             <input type="text" id="state-input"></input>
-            <label for="state-input">State</label>
+            <label for="state-input">State:</label>
           </div>
           <div className='form-flex'>
             <input type="text" id="zip-input"></input>
