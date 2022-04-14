4e51c0d6172df6731cead2f1d36c38421b877a37
ClareSegrue <cesegrue@gmail.com>
2022-04-14 03:48:32 -0400

    Update index.js

--- hw-one/hw-one-dark/src/index.js
+++ hw-one/hw-one-dark/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 
-import { render } from "react-dom";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
