5e54b41dad81c5d2396f8af7190ff7430c235639
ClareSegrue <cesegrue@gmail.com>
2022-05-04 02:05:09 -0400

    I did a lot

--- hw-one/hw-one-dark/src/components/SoundcloudPlayer.js
+++ hw-one/hw-one-dark/src/components/SoundcloudPlayer.js
@@ -4,73 +4,117 @@ import { Link, useHistory, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 
 import React, { useState, Component } from "react";
+import Accordion from "./Accordion";
 
 class SoundCloudPlayer extends Component {
   constructor(props) {
     super(props);
 
+    this.genre = null;
+    this.decade = null;
     //this.Track = new Object ();
-
-   
 
     this.songTracks = [
       {
         name: "Stressed Out",
         artist: "twenty one pilots",
-        genre: "Alternative",
+        genre: "rock",
         decade: "'10s",
         url: "https://soundcloud.com/fueled_by_ramen/twenty-one-pilots-stressed-out?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
       },
       {
         name: "Ode To Sleep",
         artist: "twenty one pilots",
-        genre: "Alternative",
-        decade: "'10s",
+        genre: "rock",
         url: "https://soundcloud.com/fueled_by_ramen/ode-to-sleep?in=fueled_by_ramen/sets/vessel&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
       },
       {
         name: "Carry On",
         artist: "Fun.",
-        genre: "Alternative",
+        genre: "rock",
         decade: "'10s",
         url: "https://soundcloud.com/fueled_by_ramen/fun-carry-on?in=fueled_by_ramen/sets/fun-some-nights&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
       },
       {
         name: "We Are Young",
         artist: "Fun.",
-        genre: "Alternative",
+        genre: "rock",
         decade: "'10s",
         url: "https://soundcloud.com/fueled_by_ramen/we-are-young-ft-janelle-monae?in=fueled_by_ramen/sets/fun-some-nights&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      },
+      {
+        name: "Bohemian Rhapsody",
+        artist: "Queen",
+        genre: "rock",
+        decade: "'70s",
+        url: "https://soundcloud.com/rizky-rilos/queen-bohemian-rhapsody?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      },
+      {
+        name: "Shake it Off",
+        artist: "Taylor Swift",
+        genre: "pop",
+        decade: "'10s",
+        url: "https://soundcloud.com/suarga/taylor-swift-shake-it-off?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      },
+      {
+        name: "Dark Horse",
+        artist: "Katy Perry",
+        genre: "pop",
+        decade: "'10s",
+        url: "https://soundcloud.com/katyperry/dark-horse-feat-juicy-j?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      },
+      {
+        name: "Before He Cheats",
+        artist: "Carrie Underwood",
+        genre: "country",
+        decade: "'00s",
+        url: "https://soundcloud.com/carrieunderwoodofficial/before-he-cheats-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
       },
     ];
 
     this.tracksToRender = this.tracks;
 
-    this.url =
-      "https://soundcloud.com/fueled_by_ramen/twenty-one-pilots-stressed-out?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
-    //const openSections = {};
+    this.url = this.songTracks[0].url;
 
-    //this.state = { openSections };
+    this.player = null;
+    this.tracks = null;
+    this.visibleTracks = this.songTracks;
+
+    this.favoritesList = [];
+
+    this.state = this.player = (
+      <ReactPlayer
+        width="100%"
+        height="100px"
+        url={this.url}
+        video="none"
+      ></ReactPlayer>
+    );
+
+    this.favorites = null;
+
+    this.state = this.favorites = <div>No current favorites.</div>;
+
+    this.updateFavorites = () => {
+      this.renderedOutput = this.favoritesList.map((item) => <li>{item}</li>);
+
+      this.setState(
+        (this.favorites = (
+          <div className="favorites">{this.renderedOutput}</div>
+        ))
+      );
+    };
 
     this.tracks = (
       <div className="main-panel">
         <div
           className="track"
           onClick={() => {
-            this.url =
-              this.songTracks[0].url;
-
-            this.changeTrack();
+            window.open("https://www.kelloggs.com/en_US/home.html", "_blank");
           }}
         >
-          {this.songTracks[0].name}
+          Cereal Flakes, Asbestos Free!
         </div>
-        <div className="track"           onClick={() => {
-            window.open(
-              "https://www.kelloggs.com/en_US/home.html",
-              "_blank"
-            );
-          }}>Cereal Flakes, Asbestos Free!</div>
         <div
           className="track"
           onClick={() => {
@@ -81,6 +125,17 @@ class SoundCloudPlayer extends Component {
           }}
         >
           Ode to Sleep
+          <button
+            className="light-button"
+            id="default-button"
+            onClick={() => {
+              this.favoritesList.push("Ode to Sleep");
+
+              this.updateFavorites();
+            }}
+          >
+            favorite
+          </button>
         </div>
         <div
           className="track"
@@ -92,6 +147,17 @@ class SoundCloudPlayer extends Component {
           }}
         >
           Carry On
+          <button
+            className="light-button"
+            id="default-button"
+            onClick={() => {
+              this.favoritesList.push("Carry On");
+
+              this.updateFavorites();
+            }}
+          >
+            favorite
+          </button>
         </div>
         <div
           className="track"
@@ -114,6 +180,17 @@ class SoundCloudPlayer extends Component {
           }}
         >
           We Are Young.
+          <button
+            className="light-button"
+            id="default-button"
+            onClick={() => {
+              this.favoritesList.push("We Are Young.");
+
+              this.updateFavorites();
+            }}
+          >
+            favorite
+          </button>
         </div>
         <div
           className="track"
@@ -128,82 +205,135 @@ class SoundCloudPlayer extends Component {
         </div>
       </div>
     );
-
-    this.state = (
-      <ReactPlayer
-        width="100%"
-        height="100px"
-        url={this.url}
-        video="none"
-      ></ReactPlayer>
-    );
   }
 
   changeTrack = () => {
     this.setState(
-      <ReactPlayer
-        width="100%"
-        height="100px"
-        url={this.url}
-        video="none"
-      ></ReactPlayer>
+      (this.player = (
+        <ReactPlayer
+          width="100%"
+          height="100px"
+          url={this.url}
+          video="none"
+        ></ReactPlayer>
+      ))
     );
   };
 
-  changeVisibleTracks = () => {};
-  /*
-  onClick = label => {
-    const {
-      state: { openSections },
-    } = this;
-
-    const isOpen = !!openSections[label];
-
-    this.setState({
-      url: ""
+  changeVisibleTracks = () => {
+    this.tempTracks = [];
+    this.songTracks.map((item) => {
+      //alert("test: " + item.name + " item genre " + item.genre + " vs. " + this.genre);
+      //alert("item genre" + this.item.genre);
+      if (item.genre === this.genre) {
+        this.tempTracks.push(item);
+      } else if (this.genre === null) {
+        this.tempTracks = this.songTracks;
+      }
+      //alert(this.tempTracks.name);
     });
+
+    this.renderedOutput = this.tempTracks.map((item) => (
+      <div
+        className="track"
+        onClick={() => {
+          this.url = item.url;
+          this.changeTrack();
+        }}
+      >
+        {item.name} <br></br> {item.artist} <br></br> {item.genre}
+        <button
+          className="light-button"
+          id="default-button"
+          onClick={() => {
+            this.favoritesList.push(item.name);
+
+            this.updateFavorites();
+          }}
+        >
+          favorite
+        </button>
+      </div>
+    ));
+
+    this.setState(
+      (this.tracks = (
+        <div className="main-panel">
+          <div className="tracks">{this.renderedOutput}</div>
+        </div>
+      ))
+    );
   };
-  */
 
   render() {
-    /*const {
-      onClick,
-      props: { children },
-      state: { url },
-    } = this;
-    */
-
     return (
       <div className="flex-table" id="gif-container">
-        <div>
-          <div className="flex-table">
-            <div className="main-panel">{this.tracks}</div>
-            <div className="left-panel">
-              <div className="filter">
-                <div className="filter-description">Genre:</div>
-                <div className="filter-select">
-                  <select onChange={this.changeVisibleTracks()}>
-                    <option value="rock">Rock</option>
-                    <option value="country">Country</option>
-                    <option value="pop">Pop</option>
-                  </select>
-                </div>
+        <div className="flex-table">
+          {this.tracks}
+          <div className="left-panel">
+            library
+            <Accordion>
+              <div className="accordion-header" label="favorites">
+                {this.favorites}
               </div>
-              <div className="filter">
-                <div className="filter-description">Decade:</div>
-                <div className="filter-select">
-                  <select>
-                    <option value="90">'90s</option>
-                    <option value="00">'00s</option>
-                    <option value="10">'10s</option>
-                  </select>
+              <div className="accordion-header" label="filters">
+                <div className="filter">
+                  <div className="filter-description">Genre:</div>
+                  <div className="filter-select">
+                    <select
+                      onChange={(e) => {
+                        this.genre = e.target.value;
+                        alert(this.genre);
+                        this.changeVisibleTracks();
+                      }}
+                    >
+                      <option value="rock">Rock</option>
+                      <option value="country">Country</option>
+                      <option value="pop">Pop</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="filter"></div>
               </div>
+            </Accordion>
+            <div
+              id="black"
+              onClick={() => {
+                window.open(
+                  "https://www.kelloggs.com/en_US/home.html",
+                  "_blank"
+                );
+              }}
+            >
+              Cereal Flakes, Asbestos Free!
             </div>
-            <header className="bottom-header">
-              <div className="player">{this.state}</div>
-            </header>
+            <div
+          id="black"
+          onClick={() => {
+            window.open(
+              "http://www.dolekemp96.org/agenda/issues/internet.htm",
+              "_blank"
+            );
+          }}
+        >
+          Zombie Bob Dole 2024
+        </div>
+            <div
+              id="black"
+              onClick={() => {
+                window.open(
+                  "https://www.jimsautorepaircenter.com/services/brakes",
+                  "_blank"
+                );
+              }}
+            >
+              Jim’s Brake Pads, “They work every time, 50% of the time”
+            </div>
           </div>
+
+          <header className="bottom-header">
+            <div className="player">{this.player}</div>
+          </header>
         </div>
       </div>
     );
--- hw-one/hw-one-dark/src/routes/four/lightFour.js
+++ hw-one/hw-one-dark/src/routes/four/lightFour.js
@@ -1,105 +1,30 @@
 import "../../styles/App.css";
 import logo from "../../Images/star.png";
 import { Link, useHistory, useParams } from "react-router-dom";
-import pause from "../../Images/pause.png";
-import play from "../../Images/play.png";
-
-import backward from "../../Images/backward.png";
-import forward from "../../Images/forward.png";
-
-
-
+import ReactPlayer from "react-player";
 
 import React, { useState, Component } from "react";
+import AudioPlayer from "../../components/SoundcloudPlayer";
 
-const LightFour = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const audio = new Audio();
-
-
-
-
+const DarkFour = () => {
   //const firstClick = new Boolean(true);
   //var isPlaying = new Boolean(false);
-  var track = "No track selected.";
+  var track =
+    "https://soundcloud.com/w2cvgtfgvyj1/fear?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
 
-  // used after song ends
-  const [isPlaying, setPlaying] = React.useState(false);
-  const togglePlaying = () => setPlaying(!isPlaying);
+  const changeUrl = () => {
+    var url =
+      "https://soundcloud.com/fueled_by_ramen/twenty-one-pilots-stressed-out?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+    track = url;
+  };
 
-  const [toggled, setToggled] = React.useState(false);
-  const toggleImage = () => {
-    setToggled(!toggled);
-    //setPlaying(!isPlaying);
+  /*
+  function changeTrack(url){
+    var url = "https://soundcloud.com/fueled_by_ramen/twenty-one-pilots-stressed-out?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+    track = url;
   }
+  */
 
-  const [trackSelected, setTrack] = React.useState(false);
-  const toggleTrackSelected = () => setTrack(!trackSelected);
-
-  function setPlayState() {
-    if (trackSelected) {
-      audio.pause();
-      audio = new Audio();
-      audio.play();
-      toggleImage();
-    } else {
-      alert("No track selected.");
-    }
-  }
-
-  function audioStarter() {
-    alert("reached start, should be false: " + trackSelected);
-    //toggleTrackSelected();
-
-    //alert("toggled track selected: " + trackSelected);
-    if (!isPlaying) {
-      audio.volume = 0.5;
-      audio.loop = true;
-      audio.play();
-    }
-
-    //togglePlaying();
-  }  
-  
-  function changeTrack(a){
-    //audio.pause();
-    audio = a;
-    track = "track selected";
-    toggleImage();
-    audioStarter();
-  };
- /*
-  const changeTrack = (Audio) => {
-    audio = a;
-    track = "track selected";
-    toggleImage();
-    audioStarter();
-  };
-
-
-
-
-
-
-  const changeTrack = () => {
-    track = "track selected";
-    toggleImage();
-    audioStarter();
-  };
-
- 
-  function audioStateManager(){
-    if (isPlaying) {
-      audio.pause = true;
-      audio.currentTime = 0;
-      isPlaying = false;
-    } else {
-      audio.volume = 0.6;
-    }
-   
-  };
-*/
   return (
     <div className="App">
       <header className="App-header">
@@ -115,48 +40,10 @@ const LightFour = () => {
           <img src={logo} width="32" height="32" alt="x"></img>
         </div>
       </header>
-
-      <div className="flex-table" id="gif-container">
-        <div>
-          <div>
-            Genre: 
-            <select>
-              <option value="fruit">Rock</option>
-              <option value="vegetable">Alternative</option>
-              <option value="meat">Country</option>
-              <option value="meat">Pop</option>
-            </select>
-          </div>
-          <div className="flex-table">
-            <div className="main-panel">
-              <div className="track" onClick={() => "nope"}>
-                track one
-              </div>
-              <div className="track">track two</div>
-              <div className="track">track three</div>
-              <div className="track">track four</div>
-              <div className="track">track five</div>
-            </div>
-            <header className="bottom-header">
-              <div className="player">
-                <button id="default-button">
-                  <img src={backward} alt="pause" height="30px" />
-                </button>
-                <button id="default-button" onClick={setPlayState}>
-                  {toggled && <img src={pause} alt="pause" height="50px" />}
-                  {!toggled && <img src={play} alt="play" height="50px" />}
-                </button>
-                <button id="default-button">
-                  <img src={forward} alt="pause" height="30px" />
-                </button>
-              </div>
-              <div className="trackName">{track}</div>
-            </header>
-          </div>
-        </div>
-      </div>
+     
+      <AudioPlayer />
     </div>
   );
 };
 
-export default LightFour;
+export default DarkFour;
